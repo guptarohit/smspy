f4252dfe1e7c73ace9d64c5103e22cf40bdefa10
Rohit Gupta <rohitg.tech@gmail.com>
2018-02-15 00:52:08 +0530

    v0.4.0
    - pyhton 2 classifiers
    - development status to beta
    - remove extra import

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import os
-import sys
 from codecs import open
 from setuptools import setup
 
@@ -8,23 +7,28 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = '\n' + f.read()
 
-
 with open('requirements.txt') as f:
     required = f.readlines()
 
-
 setup(
     name='way2sms',
-    version='0.3.0',
+    version='0.4.0',
     description='A python module to send sms via way2sms.com.',
     long_description=long_description,
     author='Rohit Gupta',
     url='https://github.com/guptarohit/way2sms',
     py_modules=['way2sms'],
     install_requires=required,
-    license='MIT',
+    license='BSD',
     classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Utilities',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
