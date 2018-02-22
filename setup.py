f7b85f41c4dc364a88d8c797529b58315aa61eed
Rohit Gupta <guptarohit@users.noreply.github.com>
2018-02-22 22:42:14 +0530

    Add upload command.
    This setup.py format inspired from Kenneth's projects. :)

--- setup.py
+++ setup.py
@@ -1,30 +1,65 @@
+import io
 import os
 import sys
-from codecs import open
-from setuptools import setup
+from shutil import rmtree
+from setuptools import setup, Command
+
+# Package meta-data.
+NAME = 'smspy'
+DESCRIPTION = 'A python module for sending free sms via website way2sms.'
+URL = 'https://github.com/guptarohit/smspy'
+AUTHOR = 'Rohit Gupta'
+VERSION = '0.5.0'
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = '\n' + f.read()
 
-with open('requirements.txt') as f:
+with io.open('requirements.txt') as f:
     required = f.readlines()
 
-if sys.argv[-1] == "publish":
-    os.system("python setup.py sdist bdist_wheel upload")
-    sys.exit()
 
-NAME = 'smspy'
-VERSION = '0.5.0'
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds...')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution...')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine...')
+        os.system('twine upload dist/*')
+
+        sys.exit()
+
 
 setup(
     name=NAME,
     version=VERSION,
-    description='A python module for sending free sms via website way2sms.',
+    description=DESCRIPTION,
     long_description=long_description,
-    author='Rohit Gupta',
-    url='https://github.com/guptarohit/smspy',
+    author=AUTHOR,
+    url=URL,
     packages=[NAME],
     install_requires=required,
     include_package_data=True,
@@ -43,4 +78,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'
     ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
